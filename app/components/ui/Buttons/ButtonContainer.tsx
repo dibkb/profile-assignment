fe1e939f1ad44a3b0b4a80c86992c7ce43fd85ed
dibkb <dibas9110@gmail.com>
2024-08-17 01:02:53 +0530

    navabr resoponsive

--- app/components/ui/Buttons/ButtonContainer.tsx
+++ app/components/ui/Buttons/ButtonContainer.tsx
@@ -1,15 +1,27 @@
 "use client";
+import { cn } from "@/utils";
 import React from "react";
 
 interface ButtonContainer {
   number: number;
   title: string;
   Svg: React.ReactNode;
+  className?: string;
 }
-const ButtonContainer = ({ title, number, Svg }: ButtonContainer) => {
+const ButtonContainer = ({
+  title,
+  number,
+  Svg,
+  className,
+}: ButtonContainer) => {
   return (
-    <div className="flex flex-col items-center justify-center py-1 gap-1 text-xs text-white cursor-pointer rounded-md w-20  hover:bg-blue-500 relative">
-      <p className="absolute right-1 top-1 rounded-full bg-yellow-400 text-black px-2 font-medium">
+    <div
+      className={cn(
+        "flex flex-col items-center justify-center py-1 gap-1 text-xs text-white cursor-pointer rounded-md w-20 hover:bg-blue-500 relative",
+        className
+      )}
+    >
+      <p className="absolute right-1 top-1 rounded-full bg-yellow-400 px-2 font-medium">
         {number}
       </p>
       {Svg}
--- app/components/ui/Navbar.tsx
+++ app/components/ui/Navbar.tsx
@@ -7,43 +7,79 @@ import ButtonContainer from "./Buttons/ButtonContainer";
 import { CartICon } from "./svg/Cart";
 import { Heart } from "./svg/Heart";
 import { Bars3 } from "./svg/Bars3";
+import { Xmark } from "./svg/Xmark";
 export const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
   return (
     <main className={`bg-blue-600 h-[64px]`}>
       <div className="w-[90%] sm:container mx-auto flex items-center justify-between gap-4 h-full">
         {/* mobile */}
-
         <img
           src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png"
           alt=""
           width={70}
         />
-        <button className="flex items-center min-w-7 md:hidden">
-          <Bars3 className="text-white" />
+        <button
+          className={`flex items-center min-w-7 md:hidden transition-transform transform duration-300 ${
+            navbarOpen ? "rotate-180" : "rotate-0"
+          }`}
+          onClick={() => setNavbarOpen((p) => !p)}
+        >
+          {navbarOpen ? (
+            <Xmark className="text-white" />
+          ) : (
+            <Bars3 className="text-white" />
+          )}
         </button>
         {/* larger screens */}
         <div className="hidden md:flex items-center justify-between flex-1">
-          {/* middle */}
-          <div className="bg-zinc-100 flex rounded-lg w-96 px-2 py-1 items-center gap-1 h-9">
-            <MagnifyingGlass className="size-5" />
-            <input
-              placeholder="Search for products, brands and more"
-              type="text"
-              className="text-zinc-800 flex-grow bg-transparent !outline-none text-sm"
-            />
-          </div>
-          {/* right */}
-          <div className="flex gap-3 items-center">
-            <ButtonContainer Svg={<CartICon />} number={4} title="Cart" />
-            <ButtonContainer
-              Svg={<Heart className="size-6" />}
-              number={4}
-              title="Wishlist"
-            />
-            <Login />
-          </div>
+          <>
+            <div className="bg-zinc-100 flex rounded-lg w-96 px-2 py-1 items-center gap-1 h-9">
+              <MagnifyingGlass className="size-5" />
+              <input
+                placeholder="Search for products, brands and more"
+                type="text"
+                className="text-zinc-800 flex-grow bg-transparent !outline-none text-sm"
+              />
+            </div>
+
+            <div className="flex gap-3 items-center">
+              <ButtonContainer Svg={<CartICon />} number={4} title="Cart" />
+              <ButtonContainer
+                Svg={<Heart className="size-6" />}
+                number={4}
+                title="Wishlist"
+              />
+              <Login />
+            </div>
+          </>
         </div>
       </div>
+      {/* mobile modal */}
+      {navbarOpen && (
+        <div className="absolute z-[1000] top-[64px] w-full left-0 h-[calc(100vh-64px)] bg-blue-600 p-4">
+          <>
+            <div className="bg-zinc-100 flex rounded-lg w-full px-2 py-1 items-center gap-1 h-9">
+              <MagnifyingGlass className="size-5" />
+              <input
+                placeholder="Search for products, brands and more"
+                type="text"
+                className="text-zinc-800 flex-grow bg-transparent !outline-none text-sm"
+              />
+            </div>
+
+            <div className="flex flex-col mt-4 gap-3 items-center">
+              <ButtonContainer Svg={<CartICon />} number={4} title="Cart" />
+              <ButtonContainer
+                Svg={<Heart className="size-6" />}
+                number={4}
+                title="Wishlist"
+              />
+              <Login />
+            </div>
+          </>
+        </div>
+      )}
     </main>
   );
 };
