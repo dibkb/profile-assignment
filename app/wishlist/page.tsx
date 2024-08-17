18c5aa56af30a7d5e7ec9d66501bded1a3045784
dibkb <dibas9110@gmail.com>
2024-08-18 01:33:45 +0530

    wishlist page

---
+++ app/wishlist/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { WishlistCard } from "../components/ui/Cart/WishlistCard";
+
+const WishlistPage = () => {
+  return (
+    <div className="bg-zinc-200 p-2 min-h-[calc(100vh-64px)]">
+      <WishlistCard />
+    </div>
+  );
+};
+
+export default WishlistPage;
