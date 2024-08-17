618b04cd2fb3ad92d4484b82a485a1d8afa4f5e3
dibkb <dibas9110@gmail.com>
2024-08-17 22:57:20 +0530

    empty cart design

--- app/components/ui/Cart/CartDetails.tsx
+++ app/components/ui/Cart/CartDetails.tsx
@@ -20,7 +20,21 @@ export const CartDetails = () => {
   });
   return (
     <CartItemLayout title="Cart Details">
-      <div className="p-4 flex flex-col gap-4">{dispalyCartItem}</div>
+      {cartItem.length ? (
+        <div className="p-4 flex flex-col gap-4">{dispalyCartItem}</div>
+      ) : (
+        <div className="flex items-center flex-col gap-2 justify-center py-6">
+          <img
+            width={300}
+            src={`https://rukminim2.flixcart.com/www/800/800/promos/16/05/2019/d438a32e-765a-4d8b-b4a6-520b560971e8.png?q=90`}
+            alt="Empty cart"
+          />
+          <p className="">Your cart is empty!</p>
+          <p className="text-xs">
+            Explore our wide selection and find something you like
+          </p>
+        </div>
+      )}
     </CartItemLayout>
   );
 };
--- app/components/ui/Cart/CartProduct.tsx
+++ app/components/ui/Cart/CartProduct.tsx
@@ -2,16 +2,45 @@
 
 import { useStoreContext } from "@/app/context/StoreContext";
 import { CartItem } from "@/types/products";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const CartProduct = ({ id, title, assured, price, image }: CartItem) => {
-  const { cart } = useStoreContext();
+  const { cart, setCart } = useStoreContext();
   const inputButtonClass = `border w-8 h-8 flex items-center justify-center rounded-full bg-zinc-200 hover:bg-zinc-300`;
   const buttonClass =
     "uppercase font-medium text-zinc-500 px-4 hover:text-zinc-600";
-  const [quantity, setQuantity] = useState(cart.get(id));
+
+  const [quantity, setQuantity] = useState(cart.get(id) ?? 0);
+
+  useEffect(() => {
+    setQuantity(cart.get(id) ?? 0);
+  }, [cart, id]);
+
+  function changeQuantityHandler(type: "increment" | "decrement") {
+    setCart((prev) => {
+      const newCart = new Map(prev);
+      const currentQuantity = newCart.get(id) ?? 0;
+
+      if (type === "increment") {
+        newCart.set(id, currentQuantity + 1);
+      } else if (type === "decrement" && currentQuantity > 1) {
+        newCart.set(id, currentQuantity - 1);
+      }
+
+      return newCart;
+    });
+  }
+
+  function removeHandler() {
+    setCart((prev) => {
+      const newCart = new Map(prev);
+      newCart.delete(id);
+      return newCart;
+    });
+  }
+
   return (
-    <main key={id}>
+    <main>
       <div className="flex gap-4 p-2">
         <div
           className="w-40 h-40"
@@ -28,6 +57,7 @@ export const CartProduct = ({ id, title, assured, price, image }: CartItem) => {
             <img
               src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/fa_62673a.png"
               width={70}
+              alt="Assured"
             />
           )}
           <p className="font-medium">₹{price}</p>
@@ -35,18 +65,31 @@ export const CartProduct = ({ id, title, assured, price, image }: CartItem) => {
       </div>
       <div className="flex gap-8 items-center">
         <div className="flex p-2 items-center gap-2">
-          <button className={inputButtonClass}>-</button>
+          <button
+            className={inputButtonClass}
+            onClick={() => changeQuantityHandler("decrement")}
+          >
+            -
+          </button>
           <input
             type="number"
             min={1}
             value={quantity}
+            readOnly
             className="w-12 p-1 flex items-center justify-between border rounded-md"
           />
-          <button className={inputButtonClass}>+</button>
+          <button
+            className={inputButtonClass}
+            onClick={() => changeQuantityHandler("increment")}
+          >
+            +
+          </button>
         </div>
         <div>
           <button className={buttonClass}>Save for later</button>
-          <button className={buttonClass}>Remove</button>
+          <button className={buttonClass} onClick={removeHandler}>
+            Remove
+          </button>
         </div>
       </div>
     </main>
