c1c8864d2d0d8551a999b5167897feb96df508f2
dibkb <dibas9110@gmail.com>
2024-08-18 01:41:07 +0530

    Navbar cleaning

--- app/components/ui/Navbar.tsx
+++ app/components/ui/Navbar.tsx
@@ -9,6 +9,7 @@ import { Heart } from "./svg/Heart";
 import { Bars3 } from "./svg/Bars3";
 import { Xmark } from "./svg/Xmark";
 import { useStoreContext } from "@/app/context/StoreContext";
+import Link from "next/link";
 export const Navbar = () => {
   const { cart, wishlist } = useStoreContext();
   const [navbarOpen, setNavbarOpen] = useState(false);
@@ -17,11 +18,14 @@ export const Navbar = () => {
     <main className={`bg-blue-600 h-[64px]`}>
       <div className="w-[90%] sm:container mx-auto flex items-center justify-between gap-4 h-full">
         {/* mobile */}
-        <img
-          src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png"
-          alt=""
-          width={70}
-        />
+        <Link href={"/"}>
+          <img
+            src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png"
+            alt=""
+            width={70}
+          />
+        </Link>
+
         <button
           className={`flex items-center min-w-7 md:hidden transition-transform transform duration-300 ${
             navbarOpen ? "rotate-180" : "rotate-0"
@@ -59,7 +63,7 @@ export const Navbar = () => {
                 title="Wishlist"
                 link="wishlist"
               />
-              <Login />
+              {/* <Login /> */}
             </div>
           </>
         </div>
@@ -90,7 +94,7 @@ export const Navbar = () => {
                 title="Wishlist"
                 link="wishlist"
               />
-              <Login />
+              {/* <Login /> */}
             </div>
           </>
         </div>
